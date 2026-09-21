405b53ff4f1cee7a1aa0ee1c54dca87cdd719c27
skawldh <kubera043672@gmail.com>
2026-09-22 04:16:06 +0900

    Delete 2605007-NamYerim-VN directory

--- 2605007-NamYerim-VN/ScenceGameResult.cpp
+++
@@ -1,6 +0,0 @@
-#include "ScenceGameResult.h"
-
-int ScenceGameResult::Init() { return 0; }
-int ScenceGameResult::Destroy() { return 0; }
-int ScenceGameResult::Update(const KEYCODE* keys) { return 0; }
-int ScenceGameResult::Render() { return 0; }
